956038a02271255515c444a8ab3591b6c16fb6be
Heitor Rosa <heitorrs.dev@gmail.com>
2025-05-17 14:13:25 -0300

    feat: creation of a auth middleware

---
+++ backend/middlewares/authMiddleaWare.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
+
+  if (!token) return res.status(401).json({ message: "Token não fornecido" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = { id: decoded.id };
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token inválido" });
+  }
+};
+
+export default authMiddleware;
