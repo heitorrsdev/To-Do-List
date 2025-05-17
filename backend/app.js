cba6cbccc22de59a30becf2e721ce33aa5d5356e
Heitor Rosa <heitorrs.dev@gmail.com>
2025-05-17 14:49:01 -0300

    feat: user routes added to express

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,18 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import taskRoutes from "./routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", taskRoutes);
 
-// Conecte ao banco
+app.use("/api", taskRoutes);
+app.use("/api", userRoutes);
+
+// Conexão com o banco
 mongoose.connect("mongodb://localhost:27017/todolist").then(() => {
   console.log("Conectado ao MongoDB");
   app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
