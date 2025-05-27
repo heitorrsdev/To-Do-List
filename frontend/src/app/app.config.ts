e97d28cb1fdb39db871bed4c124faeead9c8bac0
Heitor Rosa <heitorrs.dev@gmail.com>
2025-05-27 12:17:03 -0300

    feat: defined the routes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
+import { AppRoutes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
 import { authInterceptor } from './core/services/auth.interceptor';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    AppRoutes
   ]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { TaskListComponent } from './pages/tasks/task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
+
+export const AppRoutes = provideRouter(routes);
