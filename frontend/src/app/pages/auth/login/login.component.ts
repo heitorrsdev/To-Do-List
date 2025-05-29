cec94c6bb69f12a8bff58db8d5d5ce64380d5b3d
Heitor Rosa <heitorrs.dev@gmail.com>
2025-05-28 23:53:57 -0300

    feat: login component logic

--- frontend/src/app/pages/auth/login/login.component.ts
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { Credentials } from '../../../core/models/credentials.model';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule // Necessário para routerLink
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup; // o ! indica que o campo será inicializado posteriormente
+  errorMessage: string | null = null;
 
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    this.errorMessage = null; // Reseta a messagem de erro
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched(); // Marca todos os campos como tocados para exibir erros de validação
+      return;
+    }
+
+    const credentials: Credentials = this.loginForm.getRawValue();
+
+    this.authService.login(credentials).subscribe({
+      next: () => {
+        // Navega para a página inicial após o login bem-sucedido
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        // Mostra a mensagem de erro
+        this.errorMessage = err.error?.message || 'Falha no login. Verifique suas credenciais.';
+        console.error('Login failed:', err);
+      }
+    });
+  }
 }
+
