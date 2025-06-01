0c1721c30f291eb97ee570e17db8c7ca02c66ebc
Heitor Rosa <heitorrs.dev@gmail.com>
2025-06-01 11:59:52 -0300

    feat: using the authGuard to '' route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,24 @@
-import { Routes } from '@angular/router';
-import { provideRouter } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { Routes } from '@angular/router';
 import { TaskListComponent } from './pages/tasks/task-list/task-list.component';
+import { authGuard } from './core/guards/auth.guard';
+import { provideRouter } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', component: TaskListComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  {
+    path: '',
+    component: TaskListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
 ];
 
 export const AppRoutes = provideRouter(routes);
