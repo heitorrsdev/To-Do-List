94a9a4a757388548b464fa95faaad092de6c1531
Heitor Rosa <heitorrs.dev@gmail.com>
2025-06-05 16:29:56 -0300

    fix: dialog is now a standalone component

--- frontend/src/app/components/dialog/dialog.component.ts
+++ frontend/src/app/components/dialog/dialog.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.css']
+  styleUrls: ['./dialog.component.css'],
+  standalone: true,
 })
 export class DialogComponent {
   @Input() open = false;
