df5ab922d8d2e6352e9f6eca475a8a6e8c17c897
Heitor Rosa <heitorrs.dev@gmail.com>
2025-06-30 23:58:06 -0300

    feat: task form component

---
+++ frontend/src/app/components/task-form/task-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ElementRef, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CreateTaskDto } from '../../core/services/task.service';
+import { TASK_TITLE_MAX_LENGTH } from '../../core/constants';
+
+@Component({
+  selector: 'app-task-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css']
+})
+export class TaskFormComponent implements OnInit {
+  @Input() isLoading = false;
+  @Output() taskSubmitted = new EventEmitter<CreateTaskDto>();
+
+  @ViewChild('titleInput') titleInput!: ElementRef<HTMLTextAreaElement>;
+
+  TASK_TITLE_MAX_LENGTH: number = TASK_TITLE_MAX_LENGTH;
+  taskForm!: FormGroup;
+
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.taskForm.invalid || this.taskForm.value.title.trim().length > this.TASK_TITLE_MAX_LENGTH) {
+      this.taskForm.markAllAsTouched();
+      return;
+    }
+
+    const newTaskData: CreateTaskDto = {
+      title: this.taskForm.value.title.trim(),
+      status: 'pending'
+    };
+
+    this.taskSubmitted.emit(newTaskData);
+    this.taskForm.reset();
+  }
+
+  resizeTextarea(textarea: HTMLTextAreaElement, maxHeight: number = 170): void {
+    textarea.style.height = 'auto';
+    const newHeight = Math.min(textarea.scrollHeight, maxHeight);
+    textarea.style.height = `${newHeight}px`;
+    textarea.style.overflowY = textarea.scrollHeight > maxHeight ? 'auto' : 'hidden';
+  }
+}
