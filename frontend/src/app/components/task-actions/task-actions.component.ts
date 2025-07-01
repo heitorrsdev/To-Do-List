71304aabc4fe5ff4885264024ea4ca1ab4566985
Heitor Rosa <heitorrs.dev@gmail.com>
2025-06-30 23:58:54 -0300

    feat: task actions component

---
+++ frontend/src/app/components/task-actions/task-actions.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-actions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-actions.component.html',
+  styleUrls: ['./task-actions.component.css']
+})
+export class TaskActionsComponent {
+  @Input() hasCompletedTasks = false;
+  @Input() isLoading = false;
+
+  @Output() deleteCompletedTasks = new EventEmitter<void>();
+
+  onDeleteCompletedTasks(): void {
+    this.deleteCompletedTasks.emit();
+  }
+}
