ba6155569fcda4a9bf0ea48a9f672093eab3cd5c
Heitor Rosa <heitorrs.dev@gmail.com>
2025-07-01 01:24:24 -0300

    feat: task item component

---
+++ frontend/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../core/services/task.service';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input() task!: Task;
+  @Output() statusChange = new EventEmitter<Task>();
+  @Output() taskSelected = new EventEmitter<Task>();
+
+  cycleStatus(): void {
+    this.statusChange.emit(this.task);
+  }
+
+  openTaskDetails(): void {
+    this.taskSelected.emit(this.task);
+  }
+}
