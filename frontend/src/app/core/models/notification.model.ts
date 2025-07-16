d74fc4e5285d865c62cd802bbabd246627905057
Heitor Rosa <heitorrs.dev@gmail.com>
2025-07-16 11:25:47 -0300

    feat: creation of a notification model

---
+++ frontend/src/app/core/models/notification.model.ts
@@ -0,0 +1,9 @@
+// models/notification.model.ts
+export type NotificationType = 'success' | 'error' | 'warning' | 'info';
+
+export interface Notification {
+  id: string;
+  type: NotificationType;
+  message: string;
+  duration?: number; // em milissegundos, para auto-fechamento
+}
