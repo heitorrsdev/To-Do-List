08412e205b730045bc2de0caf574e0477db49e63
Heitor Rosa <heitorrs.dev@gmail.com>
2025-07-18 18:54:05 -0300

    feat: add notification component

---
+++ frontend/src/app/shared/notification.component.ts
@@ -0,0 +1,53 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Notification } from '../core/models/notification.model';
+import { NotificationService } from '../core/services/notification.service';
+import { Subscription } from 'rxjs';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css'],
+  animations: [
+    trigger('notificationAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ transform: 'translateY(100%)', opacity: 0 }))
+      ])
+    ])
+  ]
+})
+export class NotificationComponent implements OnInit, OnDestroy {
+  notifications: Notification[] = [];
+  private subscription: Subscription = new Subscription();
+
+  private notificationService = inject(NotificationService);
+  // usar inject ao invés de injetar no construtor se o construtor for usado só pra injetar dependências
+
+  ngOnInit(): void {
+    this.subscription = this.notificationService.notifications$.subscribe(
+      notifications => {
+        this.notifications = notifications;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  closeNotification(id: string): void {
+    this.notificationService.removeNotification(id);
+  }
+
+  // Método auxiliar para obter a classe CSS com base no tipo de notificação
+  getNotificationClass(type: string): string {
+    return `notification-${type}`;
+  }
+}
