e4bea407fec48fb12373967b22ae48cf0932367b
Heitor Rosa <heitorrs.dev@gmail.com>
2025-07-18 18:56:52 -0300

    doc: changed error variable name on error interceptor

--- frontend/src/app/core/interceptors/error.interceptor.ts
+++ frontend/src/app/core/interceptors/error.interceptor.ts
@@ -7,16 +7,16 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router: Router = inject(Router);
 
   return next(req).pipe(
-    catchError(error => {
-      if (error.status === 401) {
+    catchError(errorRespnse => {
+      if (errorRespnse.status === 401) {
         localStorage.removeItem('token');
         if (router.url !== '/login') { // router.url !== '/login' && router.navigate(['/login']) poderia ser usado, mas o ESLint não permite
           router.navigate(['/login']);
         }
       } else {
-        console.error('HTTP Error:', error);
+        console.error('HTTP Error:', errorRespnse);
       }
-      return throwError(() => error);
+      return throwError(() => errorRespnse);
     })
   );
 };
