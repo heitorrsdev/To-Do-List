bc9f8e3904ec7efe3e05f8a271c6f72bf7e30640
Heitor Rosa <heitorrs.dev@gmail.com>
2025-07-20 10:35:48 -0300

    fix: provide animations declared on app config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,12 @@ import { AppRoutes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     AppRoutes
   ]
