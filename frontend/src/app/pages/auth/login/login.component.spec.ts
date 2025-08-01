fbd9c7c116b6565d4f690c691c82fee12baf2d96
Heitor Rosa <heitorrs.dev@gmail.com>
2025-08-01 00:10:36 -0300

    feat: refactor asynchronous tests using fakeAsync and tick

--- frontend/src/app/pages/auth/login/login.component.spec.ts
+++ frontend/src/app/pages/auth/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { Component } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
@@ -64,28 +64,27 @@ describe('LoginComponent', () => {
     });
   });
 
-  it('should navigate to "/" after successful login', (done) => {
+  it('should navigate to "/" after successful login', fakeAsync(() => {
     const router = TestBed.inject(Router);
-    const navigateSpy = spyOn(router, 'navigate');
+    const navigateSpy = spyOn(router, 'navigate').and.resolveTo(true);
 
     component.loginForm.setValue({ email: 'teste@teste.com', password: '123' });
     component.onSubmit();
 
-    setTimeout(() => {
-      expect(navigateSpy).toHaveBeenCalledWith(['/']);
-      done();
-    }, 400);
-  });
+    fixture.detectChanges();
+    tick(); // avança o relógio virtual para concluir promises e observables
 
-  it('should handle login error', (done) => {
+    expect(navigateSpy).toHaveBeenCalledWith(['/']);
+  }));
+
+  it('should handle login error', fakeAsync(() => {
     mockAuthService.login.and.returnValue(throwError(() => new Error('Erro')));
 
     component.loginForm.setValue({ email: 'teste@teste.com', password: '123' });
     component.onSubmit();
 
-    setTimeout(() => {
-      expect(component.isLoading).toBeFalse();
-      done();
-    }, 2100);
-  });
+    tick(2100);
+
+    expect(component.isLoading).toBeFalse();
+  }));
 });
