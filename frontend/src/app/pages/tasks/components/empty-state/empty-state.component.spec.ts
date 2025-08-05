0c563f2ef378591c57e4d39eb02f6499c3973e94
Heitor Rosa <heitorrs.dev@gmail.com>
2025-08-05 10:09:56 -0300

    test: add unit tests to empty state component

---
+++ frontend/src/app/pages/tasks/components/empty-state/empty-state.component.spec.ts
@@ -0,0 +1,37 @@
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EmptyStateComponent } from './empty-state.component';
+
+describe('EmptyStateComponent', () => {
+  let component: EmptyStateComponent;
+  let fixture: ComponentFixture<EmptyStateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EmptyStateComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EmptyStateComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the default message when it has no input', () => {
+    fixture.detectChanges();
+
+    const liElement = fixture.debugElement.query(By.css('li')).nativeElement;
+    expect(liElement.textContent.trim()).toBe('Nenhuma tarefa encontrada. Adicione uma nova tarefa acima.');
+  });
+
+  it('should display the input message', () => {
+    const message = 'Mensagem muito explicativa';
+    component.message = message;
+    fixture.detectChanges();
+
+    const liElement = fixture.debugElement.query(By.css('li')).nativeElement;
+    expect(liElement.textContent.trim()).toBe(message);
+  });
+});
