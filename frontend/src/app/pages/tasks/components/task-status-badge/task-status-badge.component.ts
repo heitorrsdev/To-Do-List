35bc9d1a46d70dd63bf709c57980b3654da62e83
Heitor Rosa <heitorrs.dev@gmail.com>
2025-08-09 17:57:28 -0300

    chore: alphabetize imports in TaskStatus and TaskItem components

--- frontend/src/app/pages/tasks/components/task-status-badge/task-status-badge.component.ts
+++ frontend/src/app/pages/tasks/components/task-status-badge/task-status-badge.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskStatusType } from '../../../../core/models/task.model';
 
 
