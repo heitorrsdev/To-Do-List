b37a0835cb8406813cf729d3bbed3310f34175c7
Heitor Rosa <heitorrs.dev@gmail.com>
2025-08-14 10:57:07 -0300

    chore: remove comment in auth interceptor

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -2,11 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
-/**
- * AuthInterceptor is an HTTP interceptor that adds the Authorization header
- * with the Bearer token to outgoing HTTP requests if the token is available.
- */
-
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.token;
